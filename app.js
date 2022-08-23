ddcc6a2045e5d042dc566e3244f334ec008a03de
Facundo Skako <facuskako@hotmail.com>
2022-08-23 18:29:24 -0300

    Primer fetch

--- app.js
+++ app.js
@@ -2,40 +2,46 @@ import { nuevasPreguntas } from "./data/questions.js";
 import { Cuestionario } from "./models/Cuestionario.js"
 import { UI } from "./models/UI.js"
 
+
+
 const { value: text } = await Swal.fire({
     title: 'Hola!, ingresa tu nombre!',
     input: 'text',
     inputLabel: 'Podes elegir el usuario que quieras, o tu nombre :D',
     inputPlaceholder: 'Ingresa tu Nombre',
     inputAttributes: {
-      maxlength: 10,
-      autocapitalize: 'off',
-      autocorrect: 'off'
+        maxlength: 10,
+        autocapitalize: 'off',
+        autocorrect: 'off'
     }
-  })
-  
-  if (text) {
-    Swal.fire(`Bienvenid@ !<3: ${text}`)
-  };
+})
 
-  const Toast = Swal.mixin({
+if (text) {
+    Swal.fire(`Bienvenid@ !<3: ${text}`)
+};
+
+const Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
     showConfirmButton: false,
     timer: 2000,
     timerProgressBar: true,
     didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
     }
-  })
-  
-  Toast.fire({
+})
+
+Toast.fire({
     icon: 'info',
     title: 'Recorda que podes jugar las veces que quieras!'
-  })
+})
+
+
 
 const paginaRenderizada = (quiz, ui) => {
+
+
     if (quiz.haFinalizado()) {
         ui.mostrarPuntaje(quiz.puntaje)
     } else {
--- data/questions.js
+++ data/questions.js
@@ -1,4 +1,11 @@
+
+
+
 import {Question} from "../models/Question.js"
-import {data} from "./data.js"
+
+const dataFetch = fetch('./data/data.json')
+  .then(response => response.json())
+
+const data = await dataFetch
 
 export const nuevasPreguntas = data.map(pregunta => new Question(pregunta.preguntas, pregunta.opciones, pregunta.respuesta))
